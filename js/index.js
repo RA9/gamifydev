f424163f1623f9aa58518d050d1821990ce2b38a
Carlos S. Nah <cnah27@gmail.com>
2023-12-10 01:43:32 +0000

    Add images and update metadata in index.html and manifest.webmanifest

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ import TestPage from './tys.js';
 
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/js/service-worker.js')
+    navigator.serviceWorker.register('/service-worker.js')
         .then(registration => {
             console.log('Service Worker registered with scope:', registration.scope);
         })
